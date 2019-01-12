6410d8af1421a2c2557e861247f4c8c85c1b6daa
Kai Piper <kai9987kai@gmail.com>
2019-01-12 22:30:21 +0000

    Update IPfinder.py

--- IPfinder.py
+++ IPfinder.py
@@ -1,9 +1,29 @@
 import socket
 import tkinter as tk
+from tkinter import *
+import webbrowser
+from tkinter import messagebox
+
+def callback():
+    webbrowser.open_new(r"https://github.com/kai9987kai/IPfinder/wiki")
+
+
+def callback2():
+    webbrowser.open_new(r"https://github.com/kai9987kai/IPfinder")
+
+
+def callback3():
+    webbrowser.open_new(r"https://autoclicker.webstarts.com/index.html?r=20181122215206")
+
+def clicked():
+    messagebox.showinfo('CONTACT', 'Email: kai9987kai@gmail.com')
 
 def getIp(input_url):
     ip_info = socket.gethostbyname(input_url)
     ip.config(text=ip_info)
+def EXITME():
+    exit(0)  # crashed prog so it closes
+        # strtoint("crashmE!")
 
 root = tk.Tk()
 # optionally give it a title
@@ -47,6 +67,19 @@ input_url.config(font=("Times", 18,'bold'))
 button = tk.Button(frame, text="GET IP", bg='#456bbc',fg="white",command=lambda:getIp(input_url.get()))
 button.place(x=10, y=280 , width=180)
 button.config(font=("Times", 16))
+
+menu = Menu(root)
+new_item = Menu(menu)
+new_item.add_command(label='ABOUT', command=callback)
+new_item.add_command(label='GITHUB PAGE', command=callback2)
+new_item.add_separator()
+new_item.add_command(label='GET IP', command=lambda:getIp(input_url.get()))
+new_item.add_command(label='EXIT', command=EXITME)
+menu.add_cascade(label='Menu', menu=new_item)
+new_item2 = Menu(menu)
+new_item2.add_command(label='CONTACT', command=clicked)
+menu.add_cascade(label='Help', menu=new_item2)
+root.config(menu=menu)
 
 root.resizable(False, False)
 root.iconbitmap('favicon.ico')
