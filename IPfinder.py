56f4c8ab3d0cec665317bc179e5bc0d77171b12e
Kai Piper <kai9987kai@gmail.com>
2019-02-05 21:18:09 +0000

    Update IPfinder.py

--- IPfinder.py
+++ IPfinder.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import *
 import webbrowser
 from tkinter import messagebox
-
+from tkinter import ttk
 def callback():
     webbrowser.open_new(r"https://github.com/kai9987kai/IPfinder/wiki")
 
@@ -24,6 +24,44 @@ def getIp(input_url):
 def EXITME():
     exit(0)  # crashed prog so it closes
         # strtoint("crashmE!")
+
+
+def settings():
+    window = Tk()
+    window.title("Settings")
+    window.geometry('260x50')
+    window.iconbitmap('favicon.ico')
+    window.geometry("+0+0")
+    window.attributes("-topmost", True)
+
+    def callBackFunc():
+        root.overrideredirect(True)
+        window.destroy()
+
+    def ExitWindow():
+        window.destroy()
+
+    def Full_screen():
+        root.attributes('-fullscreen', True)
+        root.bind('<Escape>', lambda e: root.destroy())
+        window.destroy()
+
+    ttk.Label(window, text="Settings").grid(column=0, row=0)
+    ttk.Button(window, text="Close", command=ExitWindow).grid(column=3, row=1)
+
+    chkValue = tk.BooleanVar()
+    chkValue.set(False)
+
+    chkExample = tk.Checkbutton(window, text='Hide Title Bar',
+                                var=chkValue, command=callBackFunc)
+    chkExample.grid(column=0, row=1)
+    chkValue2 = tk.BooleanVar()
+    chkValue2.set(False)
+
+    chkExample2 = tk.Checkbutton(window, text='FullScreen',
+                                 var=chkValue2, command=Full_screen)
+    chkExample2.grid(column=2, row=1)
+    window.mainloop()
 
 root = tk.Tk()
 # optionally give it a title
@@ -72,6 +110,7 @@ menu = Menu(root)
 new_item = Menu(menu)
 new_item.add_command(label='ABOUT', command=callback)
 new_item.add_command(label='GITHUB PAGE', command=callback2)
+new_item.add_command(label='Settings', command=settings)
 new_item.add_separator()
 new_item.add_command(label='GET IP', command=lambda:getIp(input_url.get()))
 new_item.add_command(label='EXIT', command=EXITME)
@@ -82,5 +121,7 @@ menu.add_cascade(label='Help', menu=new_item2)
 root.config(menu=menu)
 
 root.resizable(False, False)
+root.geometry("+200+200")
+root.attributes("-topmost", True)
 root.iconbitmap('favicon.ico')
 root.mainloop()
